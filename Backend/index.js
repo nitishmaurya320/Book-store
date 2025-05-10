5b2eaf3b464858d72ce88e353f696b8a94ff58ab
Nitish Maurya <nitishmaurya2255@gmail.com>
2025-05-11 02:30:35 +0530

    g

--- Backend/index.js
+++ Backend/index.js
@@ -1,33 +1,38 @@
-const express= require('express');
-const mongoose= require('mongoose');
+    const express= require('express');
+    const mongoose= require('mongoose');
 
-const dotenv= require('dotenv');
-const bookRoute= require('./route/book.route');
-const userRoute= require('./route/user.route');
-dotenv.config();
-const cors = require('cors');
-
-
-const app= express();
-app.use(express.json());
-app.use(cors());
-const PORT= process.env.PORT || 5000;
-const    MONGOURI= process.env.MONGOURI;
-//connecting to mongodb
-mongoose.connect(MONGOURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(()=>{
-    console.log("connected to mongodb")
-}).catch((err)=>{
-    console.log(err)
-})
+    const dotenv= require('dotenv');
+    const bookRoute= require('./route/book.route');
+    const userRoute= require('./route/user.route');
+    dotenv.config();
+    const cors = require('cors');
 
 
-app.use("/book", bookRoute);
-app.use("/user", userRoute);
+    const app= express();
+    app.use(express.json());
+    app.use(cors());
+    const PORT= process.env.PORT || 5000;
+    const    MONGOURI= process.env.MONGOURI;
+    //connecting to mongodb
+    mongoose.connect(MONGOURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(()=>{
+        console.log("connected to mongodb")
+    }).catch((err)=>{
+        console.log(err)
+    })
 
 
-app.listen(PORT,()=>{
-    console.log(`server is running at port ${PORT}` )
-})
+    app.use("/book", bookRoute);
+    app.use("/user", userRoute);
+
+
+    // app.listen(PORT,()=>{
+    //     console.log(`server is running at port ${PORT}` )
+    // })
+     app.listen(PORT, () => {
+    console.log(`🚀 Server is running locally on port ${PORT}`);
+  });
+
+    module.exports = app;
